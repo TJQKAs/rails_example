07da05bfc970da61f4951cc6f0f85884e481e810
TJQKAs <a26052013@gmail.com>
2015-10-15 12:29:55 +0100

    first update

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @article = Article.all
+    
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
+
+
+
+# def create
+#   @article = Article.new(params[:article])  #
+#
+#   @article.save
+#   redirect_to @article
+# end
+# end
+
+
+  # def create                                #action which saves our article
+  #   render plain: params[:article].inspect  #render method take hash with key plain
+  # end                                       #value params[:article].inspect
